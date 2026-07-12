68ce9b9320a452a4c22428c6f85c6f37d6948f2d
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-07-12 22:32:24 +0600

    Implement CRUD operations for task management

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.post('/tasks', (req, res) => {
+  // Logic to create a new task
+  res.send('Task created');
+});
+
+app.put('/tasks/:id', (req, res) => {
+  // Logic to update a task by ID
+  res.send(`Task with ID ${req.params.id} updated`);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  // Logic to delete a task by ID
+  res.send(`Task with ID ${req.params.id} deleted`);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
